0156e510bd4e0acdb971a7b0c7cc95bb3bfd6475
Jason <1046955@students.wits.ac.za>
2019-02-27 19:46:42 +0200

    fixed init

--- cell.py
+++ cell.py
@@ -1,4 +1,4 @@
 class cell:
-    def __init__(self):
-        self.type = ''
-        self.belongsToSlice = False
+    def __init__(self, type, belongsToSlice):
+        self.type = type
+        self.belongsToSlice = belongsToSlice
